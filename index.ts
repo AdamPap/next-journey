c4773dc40644caafccba3784616edb948dc00e66
AdamPap <adampapamast@hotmail.com>
2021-10-16 13:13:39 +0300

    ADD: connection to db

--- index.ts
+++ index.ts
@@ -1,11 +1,26 @@
 import express from "express";
+import { createConnection } from "typeorm";
 
-const app = express();
+const main = async () => {
+  const app = express();
 
-app.get("/", (req, res) => {
-  res.send("HOME ROUTE");
-});
+  const conn = await createConnection({
+    type: "postgres",
+    database: "trip-in-crete",
+    username: "postgres",
+    password: "postgres",
+    logging: true,
+    synchronize: true,
+    // entities:
+  });
 
-app.listen(3001, () => {
-  console.log("Server listening on PORT 3001");
-});
+  app.get("/", (req, res) => {
+    res.send("HELLO WORLD");
+  });
+
+  app.listen(3001, () => {
+    console.log("Server listening on PORT 3001");
+  });
+};
+
+main();
